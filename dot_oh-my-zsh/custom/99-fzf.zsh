9827ad79584f0bb3d63ac83785db5252192c703c
Frederic Leger <frederic@webofmars.com>
2023-03-20 16:49:20 +0100

    feat: update tools versions and global settings

--- dot_oh-my-zsh/custom/99-fzf.zsh
+++ dot_oh-my-zsh/custom/99-fzf.zsh
@@ -1,10 +1,32 @@
 export FZF_COMPLETION_OPTS="--preview 'bat --color=always --style=numbers --line-range=:500 {}'"
 
-#-----------------------------------------------------
-# use fasd_cd and fzf together
-#-----------------------------------------------------
-function z() {
-  [ $# -gt 0 ] && fasd_cd -i -d "$*" && return
-  local dir
-  dir="$(fasd -Rdl "$1" | fzf -1 -0 --no-sort +m)" && cd "${dir}" || return 1
+function fcd() {
+    if [[ "$#" != 0 ]]; then
+        builtin cd "$@";
+        return
+    fi
+    while true; do
+        local lsd=$(echo ".." && ls -p | grep '/$' | sed 's;/$;;')
+        local dir="$(printf '%s\n' "${lsd[@]}" |
+            fzf --reverse --preview '
+                __cd_nxt="$(echo {})";
+                __cd_path="$(echo $(pwd)/${__cd_nxt} | sed "s;//;/;")";
+                echo $__cd_path;
+                echo;
+                ls -p --color=always "${__cd_path}";
+        ')"
+        [[ ${#dir} != 0 ]] || return 0
+        builtin cd "$dir" &> /dev/null
+    done
 }
+
+#-----------------------------------------------------
+# use z and fzf together
+#-----------------------------------------------------
+unalias z 2> /dev/null
+z() {
+  [ $# -gt 0 ] && _z "$*" && return
+  cd "$(_z -l 2>&1 | fzf --height 40% --nth 2.. --reverse --inline-info +s --tac --query "${*##-* }" | sed 's/^[0-9,.]* *//')"
+}
+
+alias j=z
