344ed3725410c85a0d092cd110a18a9aa0331cd7
Frederic Leger <frederic@webofmars.com>
2023-04-05 00:25:34 +0200

    feat: update 2023-04-04

--- dot_oh-my-zsh/custom/99-k8s.zsh
+++ dot_oh-my-zsh/custom/99-k8s.zsh
@@ -6,10 +6,13 @@ complete -o default -F __start_kubectl k
 complete -o default -F __start_kubectl ks
 
 # kube-ps1
-function zsh-load-kubeps1() {
-  source /usr/local/Cellar/kube-ps1/0.7.0/share/kube-ps1.sh
-  export PROMPT='$(kube_ps1)'$PROMPT
-}
+if [ "$(uname -s)" = "Darwin" ]; then
+  source $(brew --prefix)/opt/kube-ps1/share/kube-ps1.sh
+else
+  source /usr/share/kube-ps1/kube-ps1.sh
+fi
+export KUBE_PS1_ENABLED=false
+export PROMPT='$(kube_ps1)'$PROMPT
 
 # krew
 export PATH="$PATH:${KREW_ROOT:-$HOME/.krew}/bin"
